53d57ae18efccb003fee3d10d83ce18334cbdb41
ihioko01 <maryihioko@gmail.com>
2023-11-29 06:10:21 -0800

    postive or negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ print("Last digit of", number, "is", last_digit, end=" ")
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
-    print("and is 0")
+    print("and is not 0")
 else:
     print("and less is than 6 and not 0")
